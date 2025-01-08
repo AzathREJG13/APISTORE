19cbc8b07bb49bff4f44a3565d5011a75768ab59
AzathREJG13 <rafaeleduardo130201@gmail.com>
2025-01-07 21:20:42 -0600

    delet_confi

--- config.py
+++
@@ -1,9 +0,0 @@
-class DevelopmentConfig:
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rafael1302:123456@localhost:5432/APISTORE'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-config = {
-    'development': DevelopmentConfig,
-}
