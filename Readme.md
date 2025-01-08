c4a774f0a9bebf704c04dbbdcd6665984626784b
AzathREJG13 <rafaeleduardo130201@gmail.com>
2025-01-07 21:02:47 -0600

    Update_routes

---
+++ Readme.md
@@ -0,0 +1,28 @@
+# API Store: Tu Tienda Online, Personalizada
+
+**Descripción:**
+
+API Store es una API RESTful desarrollada en Python (Flask) que permite gestionar un catálogo de productos de manera eficiente. Esta API ofrece operaciones CRUD (Crear, Leer, Actualizar, Eliminar) sobre los productos, y está protegida con autenticación JWT para asegurar el acceso solo a usuarios autorizados. La base de datos relacional PostgreSQL almacena toda la información de los productos.
+
+**Tecnologías Utilizadas:**
+
+* **Backend:**
+    * **Framework:** Flask (Python)
+    * **Base de datos:** PostgreSQL
+* **Frontend:**
+    * **Login:** HTML, CSS (para una interfaz de usuario básica)
+
+**Funcionalidades Clave:**
+
+* **Gestión de productos:**
+    * Crear nuevos productos
+    * Obtener información detallada de un producto
+    * Listar todos los productos
+    * Actualizar la información de un producto
+    * Eliminar un producto
+* **Autenticación JWT:**
+    * Protección de los endpoints de la API para garantizar la seguridad de los datos.
+    * Gestión de usuarios y roles (en progreso)
+
+**Cómo Utilizarla:**
+ *Proceso.........
--- src/routes/productos.py
+++ src/routes/productos.py
@@ -28,13 +28,42 @@ def get_producto(id):
     producto = Producto.query.get(id)
     if not producto:
         return jsonify({'error': 'Producto no encontrado'}), 404
-    return jsonify({
+    
+    # Datos básicos del producto
+    producto_result = {
         'id': producto.id,
         'nombre': producto.nombre,
         'descripcion': producto.descripcion,
         'precio': float(producto.precio),
         'cantidad_disponible': producto.cantidad_disponible,
-        'categoria': producto.categoria.nombre
+    }
+
+    if producto.categoria.nombre == 'Electrodomésticos':
+        producto_detalles = {
+            'consumo_energia': producto.electrodomestico.consumo_energia,
+            'garantia': producto.electrodomestico.garantia
+        }
+    elif producto.categoria.nombre == 'Telefonía':
+        producto_detalles = {
+            'marca': producto.telefonia.marca,
+            'modelo': producto.telefonia.modelo,
+            'almacenamiento': producto.telefonia.almacenamiento,
+            'ram': producto.telefonia.ram
+        }
+    elif producto.categoria.nombre == 'Alimentos':
+        producto_detalles = {
+            'fecha_caducidad': producto.alimento.fecha_caducidad,
+            'peso': producto.alimento.peso,
+            'tipo_alimento': producto.alimento.tipo_alimento
+        }
+
+    categoria_result = {
+        'id': producto.categoria.id,
+        'nombre': producto.categoria.nombre
+    }
+
+    return jsonify({
+        'producto': producto_result,
+        'detalles_producto': producto_detalles,
+        'categoria': categoria_result
     }), 200
-
-
